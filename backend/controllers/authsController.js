44952cbf1d7e49b068f84ffa85e7b6003e437e7e
udeaghad <dozie.udeagha@gmail.com>
2022-11-18 11:53:12 +0100

    Create login controller

--- backend/controllers/authsController.js
+++ backend/controllers/authsController.js
@@ -7,7 +7,7 @@ export const register = async(req, res, next) => {
 
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
-    
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
@@ -16,6 +16,23 @@ export const register = async(req, res, next) => {
 
     await newUser.save();
     res.status(200).send("User has been created successfully")
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const login = async(req, res, next) => {
+  
+  try {
+     const user = await User.findOne({username: req.body.username})
+     if(!user) return next(createError(404, "User not found"))
+     
+     const validpassword = await bcrypt.compare(req.body.password, user.password)
+     if(!validpassword) return next(createError(404, "Invalid pasword"))
+     
+     const {password, isAdmin, ...others} = user._doc;
+
+     res.status(200).json(others)
   } catch (error) {
     next(error)
   }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { register } from "../controllers/authsController.js";
+import { register, login } from "../controllers/authsController.js";
 
 const router = express.Router();
 
 router.post("/register", register)
+router.post("/login", login);
 
 export default router;
