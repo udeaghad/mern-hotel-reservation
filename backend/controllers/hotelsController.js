3bffa02e1dcc61d97cbb77e4463b8546717180f1
udeaghad <dozie.udeagha@gmail.com>
2022-11-21 08:37:47 +0100

    Update hotel controller

--- backend/controllers/hotelsController.js
+++ backend/controllers/hotelsController.js
@@ -31,7 +31,7 @@ export const deleteHotel = async(req, res, next) => {
 
 export const getHotel = async(req, res, next) => {
   try {
-    const hotel = await Hotel.findById(req.params.id);
+    const hotel = await Hotel.findById(req.params.id).populate("rooms");
     res.status(200).json(hotel)
   } catch (error) {
     next(error)
